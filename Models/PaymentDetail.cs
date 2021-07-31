41b4940f9d4c419316551949f28738e3af8980c1
YL <simon-ling@hotmail.com>
2021-07-31 12:48:29 -0400

    0705 is done

---
+++ Models/PaymentDetail.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class PaymentDetail
+    {
+        [Key]
+        public int PaymentDetailId { get; set; }
+        [Column(TypeName ="nvarchar(100)")]
+        public string CardOwnerName { get; set; }
+        [Column(TypeName ="nvarchar(16)")]
+        // mm/yy
+        public string CardNumber { get; set; }
+        [Column(TypeName ="nvarchar(5)")]
+        public string CardExpirationDate { get; set; }
+        [Column(TypeName ="nvarchar(3)")]
+        public string CardSecurityCode { get; set; }
+    }
+}
