40ea7facb9cafcdf133a3d17c231c960fd21f60d
YL <simon-ling@hotmail.com>
2021-08-01 11:47:11 -0400

    11743 is done

--- Startup.cs
+++ Startup.cs
@@ -38,11 +38,19 @@ namespace PaymentAPI
             services.AddDbContext<PaymentDetailContext>(
                 options => options.UseSqlServer(Configuration.GetConnectionString("DevConnection"))
             );
+            // CORS:cross-origin resource sharing -> allow other APIs load resources
+            services.AddCors();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
+            // Cors
+            app.UseCors(
+                options => options.WithOrigins("http://localhost:4200")
+                                    .AllowAnyMethod()
+                                    .AllowAnyHeader()
+            );
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
